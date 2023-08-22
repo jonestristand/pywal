41858372cf5f0ae3f7e6a444ae4dbbf624609322
Tristan Jones <jones.tristand@gmail.com>
2023-08-22 11:22:43 -0700

    fix: added wallpaper set for gnome dark mode

--- pywal/wallpaper.py
+++ pywal/wallpaper.py
@@ -104,6 +104,9 @@ def set_desktop_wallpaper(desktop, img):
         util.disown(["gsettings", "set",
                      "org.gnome.desktop.background",
                      "picture-uri", "file://" + urllib.parse.quote(img)])
+        util.disown(["gsettings", "set",
+                     "org.gnome.desktop.background",
+                     "picture-uri-dark", "file://" + urllib.parse.quote(img)])
 
     elif "mate" in desktop:
         util.disown(["gsettings", "set", "org.mate.background",
